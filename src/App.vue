46b0dce9c7e001e56463a71cebce77fc25aa2e67
Ognjen <opejovic@gmail.com>
2020-10-08 17:59:43 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-green-300 min-h-screen">
+  <div class="bg-red-300 min-h-screen">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
