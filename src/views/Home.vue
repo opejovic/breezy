d74d430785b2792a2ed60c550bb5c0e18b30bd6c
Ognjen <opejovic@gmail.com>
2020-10-08 23:54:55 +0200

    additional tweaks to home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home text-4xl max-w-3xl">
-    I'm trying to build some tailwind + vue components here. I'm having fun with
-    this, and I hope I can make someone elses' life easier.
+  <div class="home text-4xl max-w-3xl mx-auto">
+    I'm trying to build some Vue.js x TailwindCSS components here. First I draw
+    stuff in Figma, and I try to recreate it in tailwind. Any canvases /
+    backgrounds you may encouter here are all drawn in Figma.
 
     <br />
-    <div class="pt-4">
-      Copy and paste away. You can tweet me here - @opejovic, if you like what
-      you see.
+    <div class="pt-10">
+      I'm having fun with this, and I hope I can make someone elses' life
+      easier.
     </div>
 
     <br />
+    <div class="flex items-center space-x-4 pt-4">
+      Copy and paste away. But I could really use a greet'n'tweet @opejovic in
+      return.
+    </div>
+    <br />
     Peace,
     <br />
-    Ognjen
+    Ognjen (awg-nyen)
   </div>
 </template>
 
